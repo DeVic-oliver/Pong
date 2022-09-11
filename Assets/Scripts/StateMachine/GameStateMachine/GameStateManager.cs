96f732f4c5bbf35263590d15857cd536ddd39397
Victor Matheus <victormatheus183@gmail.com>
2022-09-11 01:03:57 -0300

    Game state manager
    It will handle with the states of the game

--- Assets/Scripts/StateMachine/GameStateMachine/GameStateManager.cs
+++ Assets/Scripts/StateMachine/GameStateMachine/GameStateManager.cs
@@ -9,21 +9,41 @@ public class GameStateManager : MonoBehaviour
     public PauseState PauseState = new PauseState();
     public RestartState RestartState = new RestartState();
     public GameOverState GameOverState = new GameOverState();
+    
+    private GameStateBase _currentState;
+    public GameManager _gameManager;
+
+
+    private void Awake()
+    {
+        SetInitialState();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InitComponents();
+    }
+
+    private void InitComponents()
+    {
+        _gameManager = GetComponent<GameManager>();
+    }
+
+    private void SetInitialState()
+    {
+        _currentState = StartState;
+        _currentState.OnStateEnter(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _currentState.UpdateState(this);
     }
-
-    public void SwitchState(GameStateBase state)
+    public void SwitchState(GameStateBase newState)
     {
-
+        _currentState = newState;
+        _currentState.OnStateEnter(this);
     }
 }
