ee4bad87b3bfeae4374a5dc291902ea411cbcfc1
Victor Matheus <victormatheus183@gmail.com>
2022-09-11 19:36:21 -0300

    Mouse cursor behaviour now is offered by interface

--- Assets/Scripts/StateMachine/GameStateMachine/Abstract Classes/PauseGameHandler.cs
+++ Assets/Scripts/StateMachine/GameStateMachine/Abstract Classes/PauseGameHandler.cs
@@ -4,5 +4,4 @@ public abstract class PauseGameHandler
     public static bool ShouldResumeGame;
     protected abstract void CheckPauseGame(GameStateManager gameState);
     protected abstract void PauseGameBehaviour();
-    protected abstract void ChangeMouseCursorBehaviour();
 }
