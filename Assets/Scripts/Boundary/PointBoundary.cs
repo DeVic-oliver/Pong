39906b6e0bc070bc63edcb00fd45cb22c12617fc
Victor Matheus <victormatheus183@gmail.com>
2022-09-10 23:59:32 -0300

    useless code deleted

--- Assets/Scripts/Boundary/PointBoundary.cs
+++ Assets/Scripts/Boundary/PointBoundary.cs
@@ -6,15 +6,6 @@ public class PointBoundary : MonoBehaviour, IScoreable
 {
     public Player Player;
     public static bool IsPointMade;
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Ball"))
-    //    {
-    //        Player.Points++;
-    //        IsPointMade = true;
-    //    }
-    //}
-
     public void ApplyScore()
     {
         Player.Points++;
