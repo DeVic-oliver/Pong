a6c1b906162bb2b9ea2f467f7bc8a0aa669657be
Victor Matheus <victormatheus183@gmail.com>
2022-09-13 20:08:48 -0300

    Set the winner of the game

--- Assets/Scripts/GameManager/GameScoreHandler.cs
+++ Assets/Scripts/GameManager/GameScoreHandler.cs
@@ -8,6 +8,13 @@ public class GameScoreHandler : MonoBehaviour
     public Player Player2Points;
     [SerializeField] private TextMeshProUGUI player1Score;
     [SerializeField] private TextMeshProUGUI player2Score;
+    [SerializeField] private TextMeshProUGUI winnerScoreboard;
+
+    private string scoreBoardInitialText;
+    private void Start()
+    {
+        scoreBoardInitialText = winnerScoreboard.text;
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,7 +27,26 @@ public class GameScoreHandler : MonoBehaviour
         player2Score.text = Player2Points.Points.ToString();
     }
     
-    public void ResetPlayerPoints()
+    public void SetGameWinner()
+    {
+        if(Player1Points.Points > Player2Points.Points) 
+        {
+            winnerScoreboard.text += " Player 1 - " + Player1Points.Points + " points";
+        }else if (Player1Points.Points < Player2Points.Points)
+        {
+            winnerScoreboard.text += " Player 2 - " + Player2Points.Points + " points";
+        }
+        else
+        {
+            winnerScoreboard.text = "Draw Game!!!";
+        }
+    }
+    public void ResetScoreboard()
+    {
+        winnerScoreboard.text = scoreBoardInitialText;
+        ResetPlayerPoints();
+    }
+    private void ResetPlayerPoints()
     {
         Player1Points.Points = 0;
         Player2Points.Points = 0;
