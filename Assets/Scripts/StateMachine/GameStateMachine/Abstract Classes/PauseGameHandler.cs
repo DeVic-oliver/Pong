9ccca09f8489e02410faa2c005a4ba823b74f3b1
Victor Matheus <victormatheus183@gmail.com>
2022-09-12 23:02:17 -0300

    Update pause game handler
    Offers an abstract method ChangePauseTimeScale that must be used to change the time scale in certain game states;
    
    Control the pause state via  ChangeGameStateBetweenPauseAndPlay method. That will navigate between pause and play method

--- Assets/Scripts/StateMachine/GameStateMachine/Abstract Classes/PauseGameHandler.cs
+++ Assets/Scripts/StateMachine/GameStateMachine/Abstract Classes/PauseGameHandler.cs
@@ -1,7 +1,36 @@
+using UnityEngine;
 public abstract class PauseGameHandler
 {
     public static bool IsGamePaused;
     public static bool ShouldResumeGame;
-    protected abstract void CheckPauseGame(GameStateManager gameState);
-    protected abstract void PauseGameBehaviour();
+    
+    /// <summary>
+    /// Change the time scale when pauses or resume game
+    /// </summary>
+    protected abstract void ChangePauseTimeScale();
+
+    /// <summary>
+    /// Change game state between the pause state and play state.
+    /// </summary>
+    /// <param name="gameState">Receives state manager</param>
+    protected void ChangeGameStateBetweenPauseAndPlay(GameStateManager gameState)
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CheckIfGameIsPaused(gameState);
+        }
+    }
+    private void CheckIfGameIsPaused(GameStateManager gameState)
+    {
+        if (!PauseGameHandler.IsGamePaused)
+        {
+            PauseGameHandler.IsGamePaused = true;
+            gameState.SwitchState(gameState.PauseState);
+        }
+        else if (PauseGameHandler.IsGamePaused)
+        {
+            PauseGameHandler.IsGamePaused = false;
+            gameState.SwitchState(gameState.PlayState);
+        }
+    }
 }
