cf1af11f58ba505864baae8e92088a63fb67d5fd
Victor Matheus <victormatheus183@gmail.com>
2022-09-13 22:34:49 -0300

    player set his name to its fields

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Player : MonoBehaviour
 {
+    public static bool shouldNameSet = false;
+
     public int Points { get; set; }
+    public string Name { get; private set; }
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TextMeshProUGUI playerName;
+    private bool isNameSet = false;
+    
+    private void Update()
+    {
+        if (shouldNameSet && !isNameSet)
+        {
+            SetPlayerName();
+            isNameSet = true;
+        }
+    }
+    private void SetPlayerName()
+    {
+        Name = nameInput.text;
+        playerName.text = Name;
+    }
 }
