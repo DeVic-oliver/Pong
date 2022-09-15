b8d58504ea1baad4bd1ea5b77c42ad9452219f65
Victor Matheus <victormatheus183@gmail.com>
2022-09-14 22:04:15 -0300

    reseting some components

--- Assets/Scripts/StateMachine/GameStateMachine/ConcreteStates/StartState.cs
+++ Assets/Scripts/StateMachine/GameStateMachine/ConcreteStates/StartState.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartState : IGameStateBase
+public class StartState : IGameStateBase, IMouseBehaviour
 {
+   
+
     public void OnStateEnter(GameStateManager gameState)
     {
+        GameStateManager.ShouldBackToMainMenu = false;
+        gameState._gameManager.CanBeginGame = false;
+
+        ChangeMouseCursorBehaviour();
+
         gameState._gameManager.DenyBallToMove();
+        gameState._gameManager._gameScoreHandler.ResetScoreboard();
+        gameState._gameManager._gameScoreHandler.Player1.ResetName();
+        gameState._gameManager._gameScoreHandler.Player2.ResetName();
+        gameState._gameManager._gameTimeHandler.ResetTime();
+
+    }
+    public void ChangeMouseCursorBehaviour()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void UpdateState(GameStateManager gameState)
